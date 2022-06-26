cf7a0f0b50fce6a02845102839b41136e61efab8
avirup <avirup.mallik@gmail.com>
2022-06-26 16:02:44 +0530

    Commiting gateway api fix

--- apigatewway/src/main/java/com/fse/apigatewway/ApiGatewayApplication.java
+++ apigatewway/src/main/java/com/fse/apigatewway/ApiGatewayApplication.java
@@ -2,12 +2,14 @@ package com.fse.apigatewway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-public class ApigatewwayApplication {
+@EnableEurekaClient
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApigatewwayApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
